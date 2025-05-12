12aa122471ed1df7d9b532ffc847acde2201af95
Paritosh Barman <70466031+ParitoshBarman@users.noreply.github.com>
2025-05-12 09:20:49 +0530

    done

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -1,5 +1,5 @@
 
-const authorizeRole = (...allowedRoles) => {
+const authorizeRole = (allowedRoles) => {
     return (req, res, next) => {
         if (!allowedRoles.includes(req.user.role)) {
             return res.status(403).json({ error: 'Permission denied' });
